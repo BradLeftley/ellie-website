26f62b398f98e30ba305a8628f0f94f381bee680
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2021-02-28 15:32:03 +0000

    change to icon

--- app/routes/icon-library.js
+++ app/routes/icon-library.js
@@ -4,103 +4,103 @@ export default Route.extend({
     model(){
         return [
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-01'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-02'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-03'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-04'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-05'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-06'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-07'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-08'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-09'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-10'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-11'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-12'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-13'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-14'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-15'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-16'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-17'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-18'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-19'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-20'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-21'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-22'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-23'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-24'
         },
         {
-            iconName: 'Something',
+            iconName: 'icon',
             src: 'ICONS-25'
         }
     ]
