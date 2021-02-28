ac87685fbf7e8949cad68caa4bc5a000db1beea9
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2021-02-28 15:28:37 +0000

    add icons

--- app/routes/icon-library.js
+++ app/routes/icon-library.js
@@ -1,6 +1,110 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(){
+        return [
+        {
+            iconName: 'Something',
+            src: 'ICONS-01'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-02'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-03'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-04'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-05'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-06'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-07'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-08'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-09'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-10'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-11'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-12'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-13'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-14'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-15'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-16'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-17'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-18'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-19'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-20'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-21'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-22'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-23'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-24'
+        },
+        {
+            iconName: 'Something',
+            src: 'ICONS-25'
+        }
+    ]
+    },
     afterModel(){
         window.scrollTo(0,0);
     }
---
+++ config/icons.js
@@ -0,0 +1,4 @@
+export default [{
+    iconName: 'Bunny',
+    src: ''
+ }]
